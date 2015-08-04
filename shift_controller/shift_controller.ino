3d33fa35e6184f058f41236e113ced40151d7055
timkrapil <tim@Tims-MacBook-Pro.local>
2015-08-03 21:59:03 -0400

    up/down buttons
    using up down incrementer instead of 4 buttons.  7 total IO pins in use

--- shift_controller/shift_controller.ino
+++ shift_controller/shift_controller.ino
@@ -2,7 +2,7 @@
 Bounce  bouncer1  = Bounce();
 Bounce  bouncer2  = Bounce();
 Bounce  bouncer3  = Bounce();
-Bounce  bouncer4  = Bounce();
+
 
 int latchpin = 5;// connect to pin 12 on the '595
 int clockpin = 7; // connect to pin 11 on the '595
@@ -11,10 +11,10 @@ int datapin = 6; // connect to pin 14 on the '595
 const int Relay1 = 2;
 const int Relay2 = 3;
 
-const int FirstButton = 10;
-const int SecondButton = 11;
-const int ThirdButton = 12;
-const int FourthButton = 13;
+const int DownButton = 10;
+const int UpButton = 11;
+
+
 
 
 int Gear = 1;
@@ -24,7 +24,7 @@ int Gear = 1;
 
 void setup() {
   // put your setup code here, to run once:
-  // Serial.begin(9600);
+  Serial.begin(9600);
   pinMode(Relay1, OUTPUT);
   pinMode(Relay2, OUTPUT);
   digitalWrite(Relay1, HIGH);
@@ -35,19 +35,19 @@ void setup() {
   pinMode(datapin, OUTPUT);
 
 
-  pinMode(FirstButton, INPUT_PULLUP);
-  pinMode(SecondButton, INPUT_PULLUP);
-  pinMode(ThirdButton, INPUT_PULLUP);
-  pinMode(FourthButton, INPUT_PULLUP);
+  pinMode(UpButton, INPUT_PULLUP);
+  pinMode(DownButton, INPUT_PULLUP);
+  
+  
 
- bouncer1.attach(FirstButton);
- bouncer2.attach(SecondButton);
- bouncer3.attach(ThirdButton);
- bouncer4.attach(FourthButton);
+ bouncer1.attach(UpButton);
+ bouncer2.attach(DownButton);
+ 
+ 
  bouncer1.interval(5);
  bouncer2.interval(5);
- bouncer3.interval(5);
- bouncer4.interval(5);
+ 
+ 
 
   Gear = 1;
   displayDigit(1);
@@ -58,21 +58,26 @@ void loop() {
  
   bouncer1.update();
   bouncer2.update();
-  bouncer3.update();
-  bouncer4.update();
+  
 
-  if (bouncer1.read() == 0){
-    Gear = 1;
+  if (bouncer1.fell()){
+    
+      if (Gear < 4){
+      Gear = Gear + 1;
+      Serial.println("+");
+      //delay(1000);
+      }
     }
-  if (bouncer2.read() == 0){
-    Gear = 2;
+  if (bouncer2.fell()){
+    
+      if (Gear > 1){
+      Gear = Gear -1;
+      Serial.println("-");
+      //delay(1000);
+      }
     }
-  if (bouncer3.read() == 0){
-    Gear = 3;
-    }
-  if (bouncer4.read() == 0){
-    Gear = 4;
-    }
+
+  
 
   
     switch (Gear) {
